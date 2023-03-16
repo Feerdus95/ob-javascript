6420daf4c1c1e86eca44444830e476dc93b6b2c3
Feerdus95 <ferdus95.fv@gmail.com>
2023-03-16 16:11:55 -0300

    Ejercicio 02

---
+++ Ejercicio-02/miPersona.js
@@ -0,0 +1,14 @@
+const miPersona = {
+    name: "Fernando",
+    age: 27,
+    isDev: true,
+    birthDate: new Date(1995,11,17),
+    favBook: {
+        bookName: "It",
+        author: "Stephen King",
+        pubDate: new Date(1986,8,15),
+        url: "www.amazon.com/-/es/Stephen-King/dp/1501142976"
+    }
+}
+
+console.log(miPersona);
