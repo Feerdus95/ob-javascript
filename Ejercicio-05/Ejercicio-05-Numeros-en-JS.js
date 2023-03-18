6213e3b695341c70c077b1e88844205d1baf84ac
Feerdus95 <ferdus95.fv@gmail.com>
2023-03-18 18:45:27 -0300

    Ejercicio 05

---
+++ Ejercicio-05/Ejercicio-05-Numeros-en-JS.js
@@ -0,0 +1,16 @@
+//Ejercicio-05-Numeros-en-JS
+//Variables
+let miAlturaInt = 163;
+let mialturaFloat = 1.66;
+let miPesoKg = 55.4;
+let valMaxJs = ((Number.MAX_VALUE + 1) === Number.MAX_VALUE);
+//Operaciones
+let miAlturaMt = mialturaFloat.toFixed(1)
+let miPesoRab = Math.round(miPesoKg)
+//Salida
+console.log(miAlturaInt)
+console.log(mialturaFloat)
+console.log(miPesoKg)
+console.log(miAlturaMt)
+console.log(miPesoRab)
+console.log(valMaxJs)
