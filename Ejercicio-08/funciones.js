108b88c01378538ff99b1bdfb7a9130f8844fd94
Feerdus95 <ferdus95.fv@gmail.com>
2023-03-20 18:36:33 -0300

    Ejercicio 08

---
+++ Ejercicio-08/funciones.js
@@ -0,0 +1,16 @@
+//Ejercicio-08-Funciones
+
+function alwaysTrue(){
+    return true;
+}
+async function promSaludo() {
+    setTimeout(()=> console.log("Hola soy una promesa"),5000)
+}
+function* evenIndexGen(){
+    let index=0;
+    while(true){
+        yield index+=2;
+    }
+}
+const evenIndex = evenIndexGen();
+
