32a8735d84db9f438890938b62648aa5d8dc1bae
Feerdus95 <ferdus95.fv@gmail.com>
2023-03-20 19:26:50 -0300

    Ejercicio 09

---
+++ Ejercicio-09/index.js
@@ -0,0 +1,21 @@
+const winston = require ('winston')
+
+const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+function tirarError(){
+  throw new Error("Funcion tirarErr0r");
+}
+try{
+  tirarError();
+} catch(e){
+  logger.log("Msj err0r!",e.toString());
+}
